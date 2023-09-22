e9d2272bbd7746f464f99e536aa2e2637104a585
ADITYA  YADAV <129522983+Aditya63a@users.noreply.github.com>
2023-09-22 20:02:40 +0530

    hii

--- js/main.js
+++ js/main.js
@@ -20,22 +20,6 @@ navBar.forEach(function (a) {
 
 //  Form Data to Googlesheet
 
-// const form = document.forms['contactform'];
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     document.querySelector("#sub").value = "Sending..";
-//     let data = new FormData(form);
-//     fetch('https://script.google.com/macros/s/AKfycbzE0F-X7rAoE75QTTVVrD73sjOPAbygSqmSEZPFwgp2iOfwWCoPoxZx3fmidDACVcULRw/exec', {
-//         method: "POST",
-//         body: data
-//     })
-//         .then(res => res.text())
-//         .then(data => {
-//             document.querySelector("#msg").innerHTML = data;
-//             document.querySelector("#sub").value = "Sending";
-//         });
-// }) 
-
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxQErFit6pIic1QxwH6kZrQ1DZ_EMKhR0gnMn0dIcOtJ8IIG53xXuk5g18jEyyxWrXHew/exec'
 
 const form = document.forms['contactform']
