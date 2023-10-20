d37d17cbc6ec2c2f90e7aebdf604d11881de8991
ADITYA  YADAV <129522983+Aditya63a@users.noreply.github.com>
2023-10-20 20:02:33 +0530

    search icon

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // header scrolled
 let nav = document.querySelector(".navbar");
 window.onscroll = function () {
-    if (document.documentElement.scrollTop > 20) {
+    if (document.documentElement.scrollTop > 30) {
         nav.classList.add("header-scrolled");
     } else {
         nav.classList.remove("header-scrolled");
